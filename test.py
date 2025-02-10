5d5ad55b0dd3e2ec53ad6517a4d9927261b2666a
juliacapella <jcapellacampde@tudelft.nl>
2025-02-10 17:52:55 +0100

    withoutradio_perish

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import pickle
 from gurobipy import Model, GRB, quicksum, gurobi
 import os
 import numpy as np
+<<<<<<< Updated upstream
 
 # Specify the full path to your Gurobi license file
 #gurobi_license_path = "/Users/mariannapiperigou/Documents/gurobi.lic"  # marianna
@@ -10,6 +11,9 @@ os.environ['GRB_LICENSE_FILE'] = '/Users/julia/Documents/OperationsOptimisation/
 # Set the environment variable
 #os.environ["GRB_LICENSE_FILE"] = gurobi_license_path
 
+=======
+os.environ['GRB_LICENSE_FILE'] = '/Users/julia/Documents/OperationsOptimisation/gurobi.lic'
+>>>>>>> Stashed changes
 #Data Extraction
 with open("B.pickle", "rb") as file1:
     bins = pickle.load(file1)
@@ -23,11 +27,18 @@ print(bins)
 Parameter Definition
 '''
 
+<<<<<<< Updated upstream
 mbins = int(sum(entry[1][2] for entry in bins.values())/2)  # number of bins -- should be halved i think
 <<<<<<< Updated upstream
 nitems = 10                                      # number of items --> why???
 =======
 nitems = 8                               # number of items --> why???
+>>>>>>> Stashed changes
+=======
+mbins = len(bins)  # number of bins -- should be halved i think
+nitems = 25# number of items --> why???
+n_axes = 2                                      # number of axes
+n_orients = 2                                   # number of different sides/orientations of an item
 >>>>>>> Stashed changes
 li = [values[0] for values in items.values()]        # length of item
 hi = [values[1] for values in items.values()]        # height of item
@@ -78,7 +89,11 @@ bcut = bins_with_cut['b']
 Model Definition
 '''
 model = Model("2DBPP")
+<<<<<<< Updated upstream
 model.setParam('TimeLimit', 60*5)
+=======
+model.setParam('TimeLimit', 3*60)
+>>>>>>> Stashed changes
 model.setParam('Method', 2)
 '''
 Variables Definition
@@ -117,6 +132,7 @@ model.update()
 Constraints Definition
 '''
 
+<<<<<<< Updated upstream
 #constraint 3: area of items not larger than area of bin
 for i in range(nitems):
     for j in range(mbins):
@@ -124,6 +140,19 @@ for i in range(nitems):
             ai[i] * p_ij[i, j] <= Aj[j] * u_j[j],
             name=f"AreaConstraint_{i}_{j}"
         )
+=======
+""" Geometric Constraints """
+
+# constraint 3: area (instead of mass) of items not larger than area of bin
+###for i in range(nitems):
+    #for j in range(mbins):
+     #   model.addConstr(
+      #      ai[i] * p_ij[i, j] <= Aj[j] * u_j[j],
+       #     name=f"AreaConstraint_{i}_{j}")
+
+for j in range(mbins):
+    model.addConstr(quicksum(ai[i] * p_ij[i, j] for i in range(nitems)) <= Aj[j] * u_j[j], name=f"TotalBinCapacity_{j}")
+>>>>>>> Stashed changes
 
 # Constraint 4: each item i is assigned to one bin j
 for i in range(nitems):
@@ -155,7 +184,16 @@ for i in range(nitems):
 #constraint 11 and 12:
 for i in range(nitems):
     # Each side aligns with exactly one axis
+<<<<<<< Updated upstream
     model.addConstr(r[i, 0, 0] + r[i, 1, 0] == 1, name=f"LengthAlign_{i}")
+=======
+    for d in range(n_orients):
+        model.addConstr(quicksum(r[i, c, d] for c in range(n_axes)) == 1, name=f"SidesAlign_{d}_{i}")
+    # Each axis aligns with exactly one side
+    for c in range(n_axes):
+        model.addConstr(quicksum(r[i, c, d] for d in range(n_orients)) == 1, name=f"AxesAlign_{c}_{i}")
+    """model.addConstr(r[i, 0, 0] + r[i, 1, 0] == 1, name=f"LengthAlign_{i}")
+>>>>>>> Stashed changes
     model.addConstr(r[i, 0, 1] + r[i, 1, 1] == 1, name=f"HeightAlign_{i}")
 
     # Each axis has exactly one side aligned
@@ -232,13 +270,99 @@ Constraints from lecture, mostly for vertical stability and cut
 
 ## Stability extra
 
+<<<<<<< Updated upstream
 #constraint 16 from lecture (flagging constraint)
 for i in range(nitems):
     for j in range(mbins):
+=======
+beta1 = model.addVars(nitems, nitems, vtype=GRB.BINARY, name='beta1')   # 1 if vertex 1 of item i is supported by item j
+beta2 = model.addVars(nitems, nitems, vtype=GRB.BINARY, name='beta2')   # 1 if vertex 2 of item i is supported by item j
+gamma = model.addVars(nitems, vtype=GRB.BINARY, name='gamma')           # 1 if vertex 1 of item i is supported by the cut of the bin where it is placed
+
+v = model.addVars(nitems, nitems, vtype=GRB.CONTINUOUS, name='k')   # represents absolute value of z_hi[k] - z_lo[i]
+m = model.addVars(nitems, nitems, vtype=GRB.BINARY, name='m')   # 1 if k overlaps or exceeds i in height, i.e. z_hi[k] > z_lo[i]
+
+#'''Vertical Stability Constraints'''
+
+#Constraint 26 adapted for 2D stability
+for i in range(nitems):
+    model.addConstr(
+        gamma[i] + quicksum(beta1[i, j] for j in range(nitems) if i!= j) + quicksum(beta2[i, j] for j in range(nitems)if i!= j) + 2*g[i]
+        >= 2
+    )
+
+for i in range(nitems):
+    # If item i is on the ground, z_lo must be small or equal to 0
+    model.addConstr(z_lo[i] <= (1 - g[i]) * H, name=f"GroundConstraint_{i}")
+    for k in range(nitems):
+        if i != k:
+            # 28&29: absolute value of height diff items i and k, v_ik can only be one if z coords at same height
+            model.addConstr(z_hi[k] - z_lo[i] <= v[i,k], name=f"AbsZ1{i}_{k}")
+            model.addConstr(z_lo[i] - z_hi[k] <= v[i,k], name=f"AbsZ2{i}_{k}")
+
+            # 30: forces m, m must be 0 if z_lo[i] larger than z_hi[k] as this means i above k
+            model.addConstr(v[i, k] <= z_hi[k] - z_lo[i] + 2 * H * (1 - m[i, k]), name=f"{k}_Below_{i}")
+            # 31: forces m, m must be 1 if z_hi[k] larger than z_lo[i] as this means i not above k
+            model.addConstr(v[i, k] <= z_lo[i] - z_hi[k] + 2 * H * m[i, k], name=f"{k}_NotBelow_{i}")
+
+            # 32: forces h, if v_ik is 0 then h must be zero (i and k height compatibility)
+            model.addConstr(h[i, k] <= v[i, k], name=f"HeightCompat_{i}_{k}")
+            # 33: forces h, if v_ik is larger than 0, then h must be 1 (no height comp.)
+            model.addConstr(v[i, k] <= h[i, k] * H, name=f"NoHeightCompat_{i}_{k}")
+
+            ####34 might need revising
+            # 34.1: forces o, if horizontal overlap, then o is 0 because neither i or k are to the right of each other
+            model.addConstr(o[i, k] <= xp[i, k] + xp[k, i], name=f"Overlap_{i}_{k}")
+            # 34.2: forces o, if no horizontal overlap, o must be 1 because either i or k are to right of each other
+            model.addConstr(xp[i, k] + xp[k, i] <= 2 * o[i, k], name=f"NoOverlap_{i}_{k}")
+
+            # 35.1: forces s=1, if hor. inters. (o=0) and/or suitable height (h=0), s must be 1 (k supports i)
+            model.addConstr((1-s[i, k]) <= h[i, k] + o[i, k], name=f"{j}_Supports_{i}_{k}")
+            # 35.2:forces s=0, if hor. inters. (o=1) and suitable height (h=1), s must be 0 (k no support i)
+            model.addConstr(h[i, k] + o[i, k] <= 2 * (1 - s[i, k]), name=f"{j}_DoesntSupport_{i}_{k}")
+
+            # 37.1: if beta1 = 1, s automatically also becomes 1
+            model.addConstr(beta1[i,k] <= s[i,k], name=f"beta1_{i}_{k}")
+            # 37.2: if beta2 = 1, s automatically also becomes 1
+            model.addConstr(beta2[i,k] <= s[i,k], name=f"beta2_{i}_{k}")
+            # 38: forces s, if beta1 or beta2 is 1, s is flagged to 1
+            model.addConstr(beta1[i, k] <= s[i, k], name=f"sBeta1Flag_{i}_{k}")
+            model.addConstr(beta2[i, k] <= s[i, k], name=f"sBeta2Flag_{i}_{k}")
+            ####39 might need revising, might not even be necessary due to 34
+            # 39.1: if i supports j (beta=1), x_l[i]>x_l[j] (eta1 = 0) and x_r[k] > x_l[i]
+            model.addConstr(eta1[i, k] <= 1 - beta1[i, k], name=f"Eta1Overlap_{i}_{k}")
+            model.addConstr(eta2[i, k] <= 1 - beta2[i, k], name=f"Eta2Overlap_{i}_{k}")
+            # 43: forces eta1 to be 1 if x_l[i] is smaller than x_l[k] (k cannot support vertex 1 of i)
+            model.addConstr(x_l[k] <= x_l[i] + eta1[i, k] * L, name=f"Eta1Flag_{i}_{k}")
+            # 45: forces eta2 to be 1 if x_r[k] is smaller than x_r[i] (k cannot support vertex 2 of i)
+            model.addConstr(x_r[i] <= x_r[k] + eta2[i, k] * L, name=f"Eta2Flag_{i}_{k}")
+            for j in range(mbins):
+                # 36: ensures s holds only for item i and item k in same bin j
+                model.addConstr(p_ij[i, j] - p_ij[k, j] <= 1-s[i, k], name=f"{k}_Supports_{i}_InSameBin1_{j}")
+                model.addConstr(p_ij[k, j] - p_ij[i, j] <= 1-s[i, k], name=f"{k}_Supports_{i}_InSameBin2_{j}")
+
+            # 49: forces gamma to be 0 if there is no cut (technically gamma could be 1 for no-cut ULD as a=b=-1)
+                model.addConstr(p_ij[i,j] + gamma[i] <= a[j] + b[j] + 3, name=f"Gamma0ForItems_{i}_inBins_{j}_NoCut_{k}")
+        for j in indices_with_cut:
+            # 47: forces gamma if item i is on cut and if i is in bin j
+            model.addConstr(z_lo[i] + b[j] / a[j] * x_l[i] - b[j] <= (1 - gamma[i]) * M + (1 - p_ij[i, j])*M,
+                            name=f"{i}_OnCutIn{j}_{k}")
+            #model.addConstr(z_lo[i] + (b[j] / a[j]) * x_l[i] - b[j] >= -M * (1 - p_ij[i,j])
+""" Other Constraints """
+#constraint 16 from lecture (flagging constraint)
+for i in range(nitems):
+    for j in range(mbins):
+        model.addConstr(u_j[j] >= p_ij[i, j], name=f"FlaggingConstraint_{i}_{j}")
+
+#some items might be fragile and, as such, no other box can be stacked on top of them
+for i in range(nitems):
+    for k in range(nitems):
+>>>>>>> Stashed changes
         model.addConstr(
             u_j[j] >= p_ij[i, j]
         )
 
+<<<<<<< Updated upstream
 #Constraint for stability
 for i in range(nitems):
     model.addConstr(
@@ -269,6 +393,17 @@ for i in range(nitems):
 #             u_j[j] >= - bcut[j]/acut[j] * x[i] + bcut[j] - (1 - p_ij[i,j]) + (1 - gamma[i])
 #         )
 
+=======
+# Ensure that a ULD cannot contain both perishable and radioactive items
+
+
+for j in range(mbins):
+    model.addConstr(
+        quicksum(p_ij[i, j] * perishable[i] for i in range(nitems)) +
+        quicksum(p_ij[i, j] * radioactive[i] for i in range(nitems)) <= 1,
+        name=f"Perishable_radioactive_{j}"
+    )
+>>>>>>> Stashed changes
 '''
 Objective Function
 '''
@@ -294,16 +429,28 @@ model.optimize()
 if model.status == GRB.INFEASIBLE:
     print("The model is infeasible. Computing IIS...")
     model.computeIIS()
+<<<<<<< Updated upstream
+=======
+    for constr in model.getConstrs():
+        if constr.IISConstr:
+            print(f"Infeasible Constraint: {constr.ConstrName}")
+    model.write("infeasible.lp")
+    model.computeIIS()
+>>>>>>> Stashed changes
     model.write("infeasible.ilp")
+
+else:
+    model.write("feasible.lp")
 
 import matplotlib.pyplot as plt
 import numpy as np
 
 def visualize_with_overlap(items, nitems, mbins, Lj, Hj, xi, zi, x_i_prime, z_i_prime, p_ij, bins_with_cut, a, b):
     fig, axs = plt.subplots(1, mbins, figsize=(15, 5))
-
+    print("Solution count:", model.SolCount)
     # Ensure the model is optimized before visualization
-    if model.status in [GRB.OPTIMAL, GRB.SUBOPTIMAL]:
+    #if model.status in [GRB.OPTIMAL, GRB.SUBOPTIMAL, GRB.TIME_LIMIT]:
+    if model.SolCount >= 0:
         for j in range(mbins):
             axs[j].set_xlim(0, Lj[j])
             axs[j].set_ylim(0, Hj[j])
@@ -365,10 +512,17 @@ def visualize_with_overlap(items, nitems, mbins, Lj, Hj, xi, zi, x_i_prime, z_i_
         print("Model didn't find a solution within the time limit.")
 
 # Call the function
+<<<<<<< Updated upstream
 if model.status == GRB.OPTIMAL or model.status == GRB.SUBOPTIMAL:
     visualize_with_overlap(items, nitems, mbins, Lj, Hj, x, z, xprime, zprime, p_ij, indices_with_cut, a, b)
+=======
+if model.status == GRB.OPTIMAL or model.status == GRB.SUBOPTIMAL or model.status == GRB.TIME_LIMIT:
+    print("Visualize")
+    visualize_with_overlap(items, nitems, mbins, Lj, Hj, x_l, z_lo, x_r, z_hi, p_ij, indices_with_cut, a, b)
+>>>>>>> Stashed changes
 else:
-    print("Model didn't find a solution within the time limit.")
+    print("Model didn't find a solution within the time limit, no visualization.")
+    print(model.status)
 
 if model.status in [GRB.OPTIMAL, GRB.SUBOPTIMAL]:
     print("\n--- Final Constraint Values ---")
@@ -388,8 +542,8 @@ if model.status in [GRB.OPTIMAL, GRB.SUBOPTIMAL]:
                     print(f"p_ij[{i},{j}]: {p_ij[i, j].X}")
 
     for j in range(mbins):
-        if u_j[j].X == 1:
-            print(f"u_j[{j}]: {u_j[j].X}")
+        #if u_j[j].X == 1:
+        print(f"u_j[{j}]: {u_j[j].X}")
 
     for i in range(nitems):
         for j in range(nitems):
